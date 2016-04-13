24bce7f4cdd3bf4ff17beb0c200224273974d738
Delian Asparouhov <me@delian.io>
2016-04-12 23:04:07 -0700

    woof4

--- lib/browser-timezone-rails.rb
+++ lib/browser-timezone-rails.rb
@@ -14,7 +14,7 @@ module BrowserTimezoneRails
     def set_time_zone(&action)
       # Use existing methods to simplify filter
       puts "TIMEZONE HEADER: " + request.headers.env["timezone"].to_s
-      puts "HEADERS: " + request.headers.env.keys
+      puts "HEADERS: " + request.headers.env.keys.to_s
       Time.use_zone(browser_timezone.presence || Time.zone, &action || request.headers.env["timezone"])
     end
 
